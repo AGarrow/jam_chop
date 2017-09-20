f796009581fc2ca94d6ac7535a9d1088e1e437f5
Alexander Garrow <alexandergarrow@Alexanders-MacBook-Pro.local>
2017-09-19 22:25:34 -0400

    carrierwave local storage

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,21 +1,24 @@
 CarrierWave.configure do |config|
-  config.storage    = :aws
-  config.aws_bucket = ENV['AWS_BUCKET']
-  config.aws_acl    = 'public-read'
+  config.storage :file
 
-  # Optionally define an asset host for configurations that are fronted by a
-  # content host, such as CloudFront.
-  # config.asset_host = 'http://example.com'
+  if Rails.env.production?
+    config.storage    = :aws
+    config.aws_bucket = ENV['AWS_BUCKET']
+    config.aws_acl    = 'public-read'
 
-  # The maximum period for authenticated_urls is only 7 days.
-  config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
+    # Optionally define an asset host for configurations that are fronted by a
+    # content host, such as CloudFront.
+    # config.asset_host = 'http://example.com'
 
-  config.aws_credentials = {
-    access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
-    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-    region:            ENV.fetch('AWS_REGION') # Required
-  }
+    # The maximum period for authenticated_urls is only 7 days.
+    config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
 
+    config.aws_credentials = {
+      access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      region:            ENV.fetch('AWS_REGION') # Required
+    }
+  end
   # Optional: Signing of download urls, e.g. for serving private content through
   # CloudFront. Be sure you have the `cloudfront-signer` gem installed and
   # configured:
