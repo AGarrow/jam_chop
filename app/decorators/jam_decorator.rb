f3efecdf161480b406d3f3288944b0855c0597d8
Alexander Garrow <alexandergarrow@Alexanders-MacBook-Pro.local>
2017-09-23 22:12:31 -0400

    track name uniqueness validation

--- app/decorators/jam_decorator.rb
+++ app/decorators/jam_decorator.rb
@@ -1,11 +1,3 @@
 class JamDecorator < Draper::Decorator
 	delegate_all
-
-	def track_errors
-		object.errors.messages.select { |k,v| k.match(/tracks\./)}.map { |m| "#{m[0].to_s.split('.')[-1].humanize} #{m[-1][0]}" }
-	end
-
-	def jam_errors
-
-	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170912001053) do
+ActiveRecord::Schema.define(version: 20170923214207) do
 
   create_table "jams", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20170912001053) do
     t.datetime "updated_at", null: false
     t.integer "track_number"
     t.boolean "download", default: true
+    t.index ["name", "jam_id"], name: "index_tracks_on_name_and_jam_id", unique: true
   end
 
 end
